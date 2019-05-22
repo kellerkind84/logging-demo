db6d830db49d19a1acdf0fddfa287617c46aa40c
Kai Kugler <kai.kugler@prodyna.com>
2019-05-22 12:45:16 +0200

    added some controllers for testing

--- src/main/java/de/example/loggingdemo/LoggingDemoApplication.java
+++ src/main/java/de/example/loggingdemo/LoggingDemoApplication.java
@@ -4,13 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-
-import java.util.Random;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
+@RestController
 public class LoggingDemoApplication {
 
     final Logger logger = LogManager.getLogger(this.getClass().getName());
@@ -38,5 +40,20 @@ public class LoggingDemoApplication {
                 logger.trace("Nothing to debug...");
                 break;
         }
+    }
+
+    @RequestMapping("/")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("hello World!");
+    }
+
+    @RequestMapping("/404test")
+    public ResponseEntity<String> notFoundTest() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @RequestMapping("/internalerrortest")
+    public ResponseEntity<String> internalErrorTest() {
+        return ResponseEntity.status(500).body("InternalServerError");
     }
 }
